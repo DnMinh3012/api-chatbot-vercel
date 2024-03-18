52c61132364443a22a1c286801838cf5cf1a0d6c
DnMinh3012 <dnhatminh1230@gmail.com>
2024-03-18 19:56:45 +0700

    update

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -107,6 +107,8 @@ function handlePostback(sender_psid, received_Postback) {
         response = { "text": "Thanks!" }
     } else if (payload === 'no') {
         response = { "text": "Oops, try sending another image." }
+    } else if (payload === 'GET_STARTED') {
+        response = { "text": "Xin chào mừng bạn" }
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
