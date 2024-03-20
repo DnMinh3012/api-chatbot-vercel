76ea61cda3536d058b25aa8e82e6c8c82a542cab
DnMinh3012 <dnhatminh1230@gmail.com>
2024-03-20 20:30:16 +0700

    update

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -26,10 +26,10 @@ let postWebhook = (req, res) => {
 
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
-            if (webhook_event.message == true) {
+            if (!webhook_event.message) {
                 handleMessage(sender_psid, webhook_event.message);
             } else
-                if (webhook_event.postback == true) {
+                if (!webhook_event.postback) {
                     console.log("12345");
                     handlePostback(sender_psid, webhook_event.postback);
 
