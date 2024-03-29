71cf02d7e21a607729a7c06209d84aac0a5510cd
DnMinh3012 <dnhatminh1230@gmail.com>
2024-03-29 22:30:36 +0700

    update

--- src/public/js/reserve-table.js
+++ src/public/js/reserve-table.js
@@ -24,63 +24,68 @@ window.extAsyncInit = function () {
 };
 
 //validate inputs
-function validateInputFields() {
-    const EMAIL_REG = /[a-zA-Z][a-zA-Z0-9_\.]{1,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}/g;
+$(document).ready(function () {
+    handleClickButtonReserveTable();
 
-    let email = $("#email");
-    let phoneNumber = $("#phoneNumber");
+    function handleClickButtonReserveTable() {
+        $("#btnReserveTable").on("click", function (e) {
+            alert("1111");
+            let check = validateInputFields(); //return true or false
 
-    if (!email.val().match(EMAIL_REG)) {
-        email.addClass("is-invalid");
-        return true;
-    } else {
-        email.removeClass("is-invalid");
+            let data = {
+                psid: $("#psid").val(),
+                customerName: $("#customerName").val(),
+                email: $("#email").val(),
+                phoneNumber: $("#phoneNumber").val()
+            };
+
+            if (!check) {
+                //close webview
+                MessengerExtensions.requestCloseBrowser(function success() {
+                    // webview closed
+                }, function error(err) {
+                    // an error occurred
+                    console.log(err);
+                });
+
+                //send data to node.js server 
+                $.ajax({
+                    url: `${window.location.origin}/reserve-table-ajax`,
+                    method: "POST",
+                    data: data,
+                    success: function (data) {
+                        console.log(data);
+                    },
+                    error: function (error) {
+                        console.log(error);
+                    }
+                });
+            }
+        });
     }
 
-    if (phoneNumber.val() === "") {
-        phoneNumber.addClass("is-invalid");
-        return true;
-    } else {
-        phoneNumber.removeClass("is-invalid");
-    }
+    //validate inputs
+    function validateInputFields() {
+        const EMAIL_REG = /[a-zA-Z][a-zA-Z0-9_\.]{1,32}@[a-z0-9]{2,}(\.[a-z0-9]{2,4}){1,2}/g;
 
-    return false;
-}
+        let email = $("#email");
+        let phoneNumber = $("#phoneNumber");
 
-
-function handleClickButtonReserveTable() {
-    $("#btnReserveTable").on("click", function (e) {
-        alert("1111")
-        let check = validateInputFields(); //return true or false
-
-        let data = {
-            psid: $("#psid").val(),
-            customerName: $("#customerName").val(),
-            email: $("#email").val(),
-            phoneNumber: $("#phoneNumber").val()
-        };
-
-        if (!check) {
-            //close webview
-            MessengerExtensions.requestCloseBrowser(function success() {
-                // webview closed
-            }, function error(err) {
-                // an error occurred
-                console.log(err);
-            });
-
-            //send data to node.js server 
-            $.ajax({
-                url: `${window.location.origin}/reserve-table-ajax`,
-                method: "POST",
-                data: data,
-                success: function (data) {
-                    console.log(data);
-                },
-                error: function (error) {
-                    console.log(error);
-                }
-            })
+        if (!email.val().match(EMAIL_REG)) {
+            email.addClass("is-invalid");
+            return true;
+        } else {
+            email.removeClass("is-invalid");
         }
-    });
-}
+
+        if (phoneNumber.val() === "") {
+            phoneNumber.addClass("is-invalid");
+            return true;
+        } else {
+            phoneNumber.removeClass("is-invalid");
+        }
+
+        return false;
+    }
+});
+
