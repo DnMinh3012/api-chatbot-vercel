79087e9c0a750b1f19208a3e9d2f5dab065de0b0
DnMinh3012 <dnhatminh1230@gmail.com>
2024-04-05 00:57:37 +0700

    update

--- src/server.js
+++ src/server.js
@@ -20,12 +20,15 @@ app.use(morgan("common"));
 // Connect to the database
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-mongoose.connect("mongodb+srv://dnhatminh1230:4iFQCp8mYqYGsAFv@chatbot.reg36mk.mongodb.net/?retryWrites=true&w=majority&appName=chatbot")
-   .then(async () => {
-      console.log("Connected to MongoDB");
-   }).catch(error => {
-      console.error("Error connecting to MongoDB:", error);
-   });
+const connectMG = async () => {
+   await mongoose.connect("mongodb+srv://dnhatminh1230:4iFQCp8mYqYGsAFv@chatbot.reg36mk.mongodb.net/?retryWrites=true&w=majority&appName=chatbot")
+      .then(async () => {
+         console.log("Connected to MongoDB");
+      }).catch(error => {
+         console.error("Error connecting to MongoDB:", error);
+      });
+}
+connectMG();
 
 
 // Configure view engine
