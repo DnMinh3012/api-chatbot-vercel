366ef1c57227662d24174437952cedc745e0a299
DnMinh3012 <dnhatminh1230@gmail.com>
2024-05-01 21:29:27 +0700

    update

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -84,15 +84,15 @@ let handleMessage = async (sender_psid, message) => {
     let response;
 
     // Checks if the message contains text
-    if (received_message.text) {
+    if (message.text) {
         // Create the payload for a basic text message, which
         // will be added to the body of our request to the Send API
         response = {
-            "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+            "text": `You sent the message: "${message.text}". Now send me an attachment!`
         }
-    } else if (received_message.attachments) {
+    } else if (message.attachments) {
         // Get the URL of the message attachment
-        let attachment_url = received_message.attachments[
+        let attachment_url = message.attachments[
             0
         ].payload.url;
         response = {
