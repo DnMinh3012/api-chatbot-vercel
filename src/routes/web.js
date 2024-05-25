1b4db5207ac737a610403a96f37b42f3811c6ebd
DnMinh3012 <dnhatminh1230@gmail.com>
2024-05-25 21:59:35 +0700

    update

--- src/routes/web.js
+++ src/routes/web.js
@@ -7,7 +7,7 @@ let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/", homepageController.getHomepage);
-    router.get("/form-reserve-table/:senderId", chatBotController.getReserveTable);
+    router.get("/form-reserve-table/:senderId/:tableTpId", chatBotController.getReserveTable);
     // router.get("/form-feedback-table/:senderId", chatBotController.getFeedbackTable);
     router.get("/webhook", chatBotController.getWebhook);
     router.post("/webhook", chatBotController.postWebhook);
