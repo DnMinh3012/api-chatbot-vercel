41d4611d003a6d354f5227607e2cf39cee1a5b8b
DnMinh3012 <dnhatminh1230@gmail.com>
2024-05-31 23:35:11 +0700

    update

--- src/services/customerService.js
+++ src/services/customerService.js
@@ -195,46 +195,51 @@ let editRevervationRequest = (reservationRequest_id, psid) => {
                     include: [
                         {
                             model: TableModel,
-                            as: "tables",
+                            as: "tables",  // Use the alias "tables" as defined in the association
                         }
                     ],
-                })
-                let response = {
-                    "attachment": {
-                        "type": "template",
-                        "payload": {
-                            "template_type": "generic",
-                            "elements": [
-                                {
-                                    "title": `Cảm ơn bạn đã đặt bàn:
-                                    \nThời gian đặt bàn của bạn là: ${reservationRequest.timeOrder}
-                                    \Tên bàn: ${reservationRequest.tables.name}`,
-                                    "image_url": IMAGE_MAIN_MENU4,
-                                    "buttons": [
-                                        {
-                                            "type": "web_url",
-                                            "url": `${process.env.URL_WEB_VIEW_ORDER}/${data.psid}`,
-                                            "title": "Thay đổi Thời Gian đặt bàn",
-                                            "webview_height_ratio": "tall",
-                                            "messenger_extensions": true
-                                        },
-                                        {
-                                            "type": "web_url",
-                                            "url": `${process.env.URL_WEB_VIEW_EDIT}/${data.psid}/${reservationRequest.id}`,
-                                            "title": "Thay đổi Thời Gian đặt bàn",
-                                            "webview_height_ratio": "tall",
-                                            "messenger_extensions": true
-                                        }
-                                    ],
-                                }
-                            ]
-                        }
-                    }
-                };
+                });
 
-                // Send the response message via chatbot service
-                await chatBotService.sendMessage(psid, response);
-                resolve("done");
+                if (reservationRequest) {
+                    let response = {
+                        "attachment": {
+                            "type": "template",
+                            "payload": {
+                                "template_type": "generic",
+                                "elements": [
+                                    {
+                                        "title": `Cảm ơn bạn đã đặt bàn:
+                                        \nThời gian đặt bàn của bạn là: ${reservationRequest.timeOrder}
+                                      `,
+                                        "image_url": IMAGE_MAIN_MENU4,
+                                        "buttons": [
+                                            {
+                                                "type": "web_url",
+                                                "url": `${process.env.URL_WEB_VIEW_ORDER}/${psid}`,
+                                                "title": "Thay đổi Thời Gian đặt bàn",
+                                                "webview_height_ratio": "tall",
+                                                "messenger_extensions": true
+                                            },
+                                            {
+                                                "type": "web_url",
+                                                "url": `${process.env.URL_WEB_VIEW_EDIT}/${psid}/${reservationRequest.id}`,
+                                                "title": "Thay đổi Thời Gian đặt bàn",
+                                                "webview_height_ratio": "tall",
+                                                "messenger_extensions": true
+                                            }
+                                        ],
+                                    }
+                                ]
+                            }
+                        }
+                    };
+
+                    // Send the response message via chatbot service
+                    await chatBotService.sendMessage(psid, response);
+                    resolve("done");
+                } else {
+                    reject("Không tìm thấy yêu cầu.");
+                }
             } else {
                 reject("Không tìm yêu cầu.");
             }
@@ -242,7 +247,8 @@ let editRevervationRequest = (reservationRequest_id, psid) => {
             reject(e);
         }
     });
-}
+};
+
 module.exports = {
     findRequestWithCustomerAndTable,
     makeReservationRequest,
