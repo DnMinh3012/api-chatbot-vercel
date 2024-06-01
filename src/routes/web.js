00ec1f8e45b63a02bc9bb6587aac5091c4ea64f2
DnMinh3012 <dnhatminh1230@gmail.com>
2024-06-01 23:33:35 +0700

    update

--- src/routes/web.js
+++ src/routes/web.js
@@ -3,17 +3,19 @@ import homepageController from "../controllers/homepageController";
 import chatBotController from "../controllers/chatBotController";
 import chatBotService from "../services/chatBotService";
 import userController from "../controllers/userController";
+const ReservationRequestModel = require('./models/ReservationRequestModel');
 let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/", homepageController.getHomepage);
     router.get('/form-reserve-table/:senderId/:tableTpId', chatBotController.getReserveTable);
-    router.get('/form-edit-table/:senderId/:tableTpId', chatBotController.getEditTable)
+    router.get('/form-edit-table/:senderId/:reservationRequestId', chatBotController.getEditTable)
     // router.get("/form-feedback-table/:senderId", chatBotController.getFeedbackTable);
     router.get("/webhook", chatBotController.getWebhook);
     router.post("/webhook", chatBotController.postWebhook);
     router.post('/setup-persistent-menu', chatBotService.setupPersistentMenu);
     router.post('/reserve-table-ajax', chatBotController.handleReserveTableAjax)
+    router.post('/edit-reserve-table-ajax', chatBotController.handleEditReserveTableAjax)
     // router.post('/feedback-table-ajax', chatBotController.handleFeedbackTableAjax)
 
     router.get('/api/get-users', userController.handleGetUsers);
@@ -24,6 +26,19 @@ let initWebRoutes = (app) => {
 
     router.get("/test", async (req, res) => {
         let user = await chatBotService.getFacebookUsername(3350311028355090);
+    });
+    app.get('/get-reservation/:id', async (req, res) => {
+        try {
+            const reservationRequestId = req.params.id;
+            const reservation = await ReservationRequestModel.findById(reservationRequestId);
+            if (reservation) {
+                res.json(reservation);
+            } else {
+                res.status(404).send('Reservation not found');
+            }
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
     });
 
     return app.use("/", router);
