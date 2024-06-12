a536f6fba885d223521736cbf7914e90db3d5ab4
quangdo2412 <100840252+quangdo2412@users.noreply.github.com>
2024-06-12 20:56:36 +0700

    Update emailService.js

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -24,14 +24,16 @@ const sendEmail = (to, subject, htmlContent) => {
 let getBodyHTMLEmail = (dataSend) => {
     let result = `
     <h3>Xin Chào ${dataSend.name}!</h3>
-    <p>Bạn nhận được email này vì đã đặt bàn Thanh Cong</p>
-    <p>Thông tin đặt bàn:</p>
+    <p>BẠN NHẬN ĐƯỢC EMAIL NÀY VÌ ĐÃ ĐẶT BÀN THÀNH CÔNG TRÊN VMU RESTAURENT</p>
+    <p>Dưới đây là thông tin của bạn.Vui lòng kiểm tra kĩ thông tin: </p>
     <div><b>Họ và tên:</b> ${dataSend.name}</div>
     <div><b>Số điện thoại:</b> ${dataSend.phoneNumber}</div>
-    <div><b>Xin cảm ơn</b></div>`;
+    <div><b>Thời gian:</b> ${dataSend.timeOrder}</div>
+    <div><b>Ghi chú:</b> ${dataSend.note}</div>
+    <div><b>Số người:</b> ${dataSend.number_of_seats}</div>
+    <div><b>Nhà hàng chúng tôi rất hân hạnh được phục vụ quý khách . Xin cảm ơn</b></div>`;
     return result;
   }
-
 
 module.exports = {
     getBodyHTMLEmail:getBodyHTMLEmail,
